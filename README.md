fa396385e34e0e1e1086df2d02c1097d84b88260
teslawebber <114813854+teslawebber@users.noreply.github.com>
2023-08-10 15:33:59 +0530

    initialize the repository and upload files

---
+++ README.md
@@ -0,0 +1,37 @@
+# My neovim only necessary things config
+
+This is my third attempt at configuring neovim. As the name suggests this version of the configuration only includes things that I would term as absolute essentials for having a neovim setup for the purpose of programming and coding.  
+This is configuration is not meant to be used for non-programmers (though they can as there are plugins that even they would benefit from).  
+The reason to keep things to at bare minimum is to keep the neovim startup time as low as possible and easy maintaibality of the config.  
+That is also the reason why I choose to keep everything in a single file.
+
+## Plugin and list of plugins used
+- Plugin manager: Lazy.nvim [lazy.nvim github repository](https://github.com/folke/lazy.nvim)  
+This is the plugin that people have been moving to from packer.nvim since it is now archived. It also has the features of lazy loading plugins withuot any configuration required by the user apart from the default.  You can also check out the [lazyvim](https://www.lazyvim.org/) if you want something that is already all setup with documentation.
+- telescope.nvim (plenary.nvim as dependency)
+- treesitter playground
+- nvim-treesitter
+- undotree
+- lsp-zero.nvim
+    - nvim-lspconfig
+    - mason.nvim
+    - manson-lspconfig.nvim
+    - nvim-cmp
+    - cmp-nvim-lsp
+    - LuaSnip
+- neovim-ayu
+
+### File searching and fuzzy finding
+[telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+For this telescope.nvim is used. It is easy to use and only has one dependency `ripgrep` which can be installed by package-manager of your operating system (yes, even on windows or macos). The default keybindings make sense based on mnemonics.
+
+### LSP and code completion
+The most simple way to setup LSP and code completion was introduced to me by ThePrimeagen and this article written by VonHeikemen who wrote the lsp-zero plugin [article](https://dev.to/vonheikemen/getting-started-with-neovims-native-lsp-client-in-the-year-of-2022-the-easy-way-bp3)
+
+### Colourscheme
+You can use any colorscheme you want. I added a colourscheme to differentiate between when I am using the shell and when I am using neovim visually without having to read anything on the screen.
+
+### Why everything in a single file?
+- To make it easy to understand.
+- I can't understand and get seperate files to work with lazy.nvim.
+- **Yeah the second reason is not compelling I know.**
---
+++ init.lua
@@ -0,0 +1,164 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+-- Auto-install lazy.nvim if not present
+if not vim.loop.fs_stat(lazypath) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
+    print('Done.')
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- packages used in neovim
+require('lazy').setup({
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'nvim-treesitter/playground' },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            run = function()
+                local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+                ts_update()
+            end,
+        }
+    },
+    {'mbbill/undotree'},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    },
+    {'Shatur/neovim-ayu'}
+})
+
+-- lsp configuration using lsp zero
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.setup()
+
+-- nvim-cmp bindings
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({ buffer = bufnr })
+end)
+
+lsp.setup()
+
+-- You need to setup `cmp` after lsp-zero
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    mapping = {
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    }
+})
+
+-- netrw appearance
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+-- remappings
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- telescope keybindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- settings for neovim appearance
+vim.cmd.colorscheme('ayu-dark')
+require('ayu').setup({
+    mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+    overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+})
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
